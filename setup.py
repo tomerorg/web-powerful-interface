dec5905020dbd614b69ce819af4d335ff8936de6
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-17 13:19:51 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="web-powerful-interface",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Experimental web project using Celery and NumPy and Pytest in Python",
+    keywords="web-powerful-interface, python",
+    url="",
+)
